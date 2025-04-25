214020087052f47e4e76ff6fea9d01b26da7f42a
Artem Kurdikov <kourtema@gmail.com>
2025-04-25 15:04:33 +0300

    feat: add React Query data fetching
    Implemented CRUD ops in client's app

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -1,8 +1,56 @@
-import { Badge, Box, Flex, Text } from "@chakra-ui/react";
+import { Badge, Box, Flex, Spinner, Text } from "@chakra-ui/react";
 import { Check, Trash2 } from "lucide-react";
 import { useColorModeValue } from "./ui/color-mode";
+import { Todo } from "./TodoList";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { BASE_URL } from "@/App";
 
-export default function TodoItem({ todo }: { todo: any }) {
+export default function TodoItem({ todo }: { todo: Todo }) {
+  const queryClient = useQueryClient();
+  
+  const { mutate: updateTodo, isPending: isUpdating } = useMutation({
+    mutationKey: ["updateTodo"],
+    mutationFn: async () => {
+      if (todo.completed) return alert("Todo is already completed!");
+      try {
+        const res = await fetch(BASE_URL + `/todos/${todo._id}`, {
+          method: "PATCH",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.message);
+        }
+        return data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    }
+  });
+
+  const { mutate: deleteTodo, isPending: isDeleting } = useMutation({
+    mutationKey: ["deleteTodo"],
+    mutationFn: async () => {
+      try {
+        const res = await fetch(BASE_URL + `/todos/${todo._id}`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.message);
+        }
+        return data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    }
+  });
+
   return (
     <Flex
       gap={2}
@@ -24,12 +72,11 @@ export default function TodoItem({ todo }: { todo: any }) {
         >
           {todo.body}
         </Text>
-        {todo.completed && (
+        {todo.completed ? (
           <Badge ml={1} colorPalette={"green"}>
             Done
           </Badge>
-        )}
-        {!todo.completed && (
+        ) : (
           <Badge ml={1} colorPalette={"yellow"}>
             In Progress
           </Badge>
@@ -41,16 +88,18 @@ export default function TodoItem({ todo }: { todo: any }) {
           color={"green.500"}
           cursor={"pointer"}
           _hover={{ bg: "green.200", borderRadius: "full" }}
+          onClick={() => updateTodo()}
         >
-          <Check />
+          {isUpdating ? <Spinner size={"md"} /> : <Check />}
         </Box>
         <Box
           p={1}
           color={"red.500"}
           cursor={"pointer"}
           _hover={{ bg: "red.200", borderRadius: "full" }}
+          onClick={() => deleteTodo()}
         >
-          <Trash2 />
+          {isDeleting ? <Spinner size={"md"} /> : <Trash2 />}
         </Box>
       </Flex>
     </Flex>
